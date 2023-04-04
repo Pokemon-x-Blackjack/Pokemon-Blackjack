ed8084c62c04a0e8a841ac9d4dda9586924395a9
Amir Abdel Rahman <meero@Amirs-MacBook-Pro.local>
2023-04-04 12:24:15 -0400

    fixed a bug that was not un-mounting the landing page component after start button has been clicked

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from 'react-router-dom';
 import { useState } from 'react';
-import characterSelector from './CharacterSelector';
+import CharacterSelector from './CharacterSelector';
 
 const Landing = () => {
 
@@ -12,27 +12,28 @@ const Landing = () => {
     }
 
     return (
-        <main className='landing'>
-          <div className="wrapper">
-            {/* ADD NAV */}
-            <div className="text">
-              <h1>POKÉJACK</h1>
-              <h3>Pokemon x Blackjack</h3>
-            </div>
-            <h3>img here</h3>
-            <div className='buttonContainer'>
-              {buttonSelected ? (
-                <CharacterSelector />
-              ) : (
+        <>
+        {buttonSelected ? (
+          <CharacterSelector />
+        ) : (
+          <main className="landing">
+            <div className="wrapper">
+              {/* ADD NAV */}
+              <div className="text">
+                <h1>POKÉJACK</h1>
+                <h3>Pokemon x Blackjack</h3>
+              </div>
+              <h3>img here</h3>
+              <div className="buttonContainer">
                 <button onClick={handleButtonClick}>START</button>
-              )}
+              </div>
+              {/* Add Link to characterSelect once component is created */}
+              {/* ADD FOOTER */}
             </div>
-            {/* Add Link to characterSelect once component is created */}
-            {/* ADD FOOTER */}
-          </div>
-        </main>
-      );
-      
+          </main>
+        )};
+      </>
+    );
 };
 
 export default Landing;
