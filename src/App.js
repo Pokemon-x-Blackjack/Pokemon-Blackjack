92966069061dbf6dd9ea32b32562c150cec6614d
Amir Abdel Rahman <meero@Amirs-MacBook-Pro.local>
2023-04-04 17:08:23 -0400

    created a Module in results.js, no logic just UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import {BrowserRouter,  Routes, Route } from 'react-router-dom';
 import Landing from './components/Landing';
-import CharacterSelector from './components/CharacterSelector';
 import Game from './components/Game';
+import Result from './components/Result';
 
 // App.js
 // Contains "Start Button"
@@ -12,6 +12,10 @@ import CharacterSelector from './components/CharacterSelector';
 function App() {
   return (
     <div className="App">
+      <Landing/>
+      <Routes>
+        <Route path="/module" element={<Result />} />
+      </Routes>
     </div>
   );
 }
