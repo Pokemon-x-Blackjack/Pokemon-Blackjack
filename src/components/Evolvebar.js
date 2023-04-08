952cf017aba17a487ca260b4368916e2c119eea5
Hadiya Hussein <you@example.com>
2023-04-08 17:40:02 -0400

    added end of game logic and game UI, created Evolvebar component and passed down appropriate props for Result and Evolvebar

---
+++ src/components/Evolvebar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Evolvebar = (props) => {
+  
+  return (
+    <div className="evolvebar-container">
+
+    </div>
+  );
+};
+
+export default Evolvebar;
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,16 +1,24 @@
 import React from "react";
 
-const Result = () => {
+const Result = (props) => {
+   console.log(props.winner)
+  
   return (
     <div className="modalContainer">
       <div className="modalContent">
-        <div className="modalTitle">Winner!</div>
+        <div className="modalTitle">        
+          {props.winner === 'player' && <p>Player wins!</p> }
+          {props.winner === 'dealer' && <p>Dealer wins!</p> }
+          {props.winner === 'ties' &&  <p>It's a tie!</p>}
+        </div>
         <button className="modalButton" >
           Back to Home
         </button>
         <button className="modalButton" >
           Choose Character
         </button>
+        
+
       </div>
     </div>
   );
