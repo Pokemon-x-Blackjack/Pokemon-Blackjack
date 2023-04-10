144fe76d6d2f394e945327fe57f5ce2908b22002
Amir Abdel Rahman <meero@Amirs-MacBook-Pro.local>
2023-04-09 23:14:05 -0400

    styled results page, added logic to render winning pokemon and winning player name on results.js component

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,26 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import Landing from "./Landing";
+import CharacterSelector from "./CharacterSelector";
 
 const Result = (props) => {
-   console.log(props.winner)
-  
-  return (
-    <div className="modalContainer">
-      <div className="modalContent">
-        <div className="modalTitle">        
-          {props.winner === 'player' && <p>Player wins!</p> }
-          {props.winner === 'dealer' && <p>Dealer wins!</p> }
-          {props.winner === 'ties' &&  <p>It's a tie!</p>}
-        </div>
-        <button className="modalButton" >
-          Back to Home
-        </button>
-        <button className="modalButton" >
-          Choose Character
-        </button>
-        
+  const [buttonSelected, setButtonSelected] = useState(false);
+  const [selectedButton, setSelectedButton] = useState(null);
 
-      </div>
-    </div>
+  const handleButtonClick = (button) => {
+    setSelectedButton(button);
+    setButtonSelected(true);
+  };
+
+  return (
+    <>
+      {!buttonSelected ? (
+        <div className="modalContainer">
+          <div className="modalContent">
+            <div className="modalTitle">
+              {props.winner === "player" && <p>You win!</p>}
+              {props.winner === "dealer" && <p>Dealer wins!</p>}
+              {props.winner === "ties" && <p>It's a tie!</p>}
+
+              {props.winner === "player" && (
+                <>
+                  <div className="imgContain">
+                    <img
+                      src={props.playerEvoArray[2].frontGifUrl}
+                      alt="Player Final Evolution Pokemon"
+                    />
+                  </div>
+                </>
+              )}
+
+              {props.winner === "dealer" && (
+                <>
+                  <div className="imgContain">
+                    <img
+                      src={props.dealerEvoArray[2].frontGifUrl}
+                      alt="Dealer Final Evolition Pokemon"
+                    />
+                  </div>
+                </>
+              )}
+              <div className="btnContain">
+                <button
+                  onClick={() => handleButtonClick("home")}
+                  className="modalButton"
+                >
+                  Back to Home
+                </button>
+                <button
+                  onClick={() => handleButtonClick("character")}
+                  className="modalButton"
+                >
+                  Choose Character
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : selectedButton === "home" ? (
+        <Landing />
+      ) : selectedButton === "character" ? (
+        <CharacterSelector />
+      ) : null}
+    </>
   );
 };
 
