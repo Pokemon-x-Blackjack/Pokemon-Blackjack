ad9f35b9ed4fa09d4fea877280f8889cb8daf3a0
Katie <katie.mcy@outlook.com>
2023-04-10 18:05:48 -0400

    added howToPlay styling

--- src/components/HowToPlay.js
+++ src/components/HowToPlay.js
@@ -5,37 +5,56 @@ import cardBack from '../assets/card-back.png';
 const HowToPlay = ({ closeHowToPlay }) => {
   return (
     <div className="howToPlayOverlay">
-      <div className="howToPlayModal">
+      <div className="howToPlayModal modalFlex">
 
-        <div className="gameRules">
+        <section className="gameRules">
           <h3>GAME RULES</h3>
             <ul>
               <li>Each round starts with two cards for you and the dealer.</li>
-              <li>Choose to hit or stand, and the dealer will hit until reaching at least 17.</li>
+              <li>Your objective is to get card value as close to 21 as you can. But Don't go over 21! Or you lose the round immediately (BUST).</li>
+              <li>Choose to <em>hit</em> or <em>stand</em>, and the dealer will hit until reaching at least 17.</li>
               <li>If your total card value is closer to 21 than the dealer's, you win and evolve your Pokemon.</li>
               <li>Keep playing to evolve your Pokemon to higher levels and become the ultimate trainer!</li>
             </ul>
-        </div>
+        </section>
       
-        <div className="cardGames">
-          
 
-          <div className="img">
-            <img src={cardBack} alt="" />
+        <div className="rightSideFlex">
+
+          <div className="cardKeyFlex">
+            <div className="img">
+              <img src={cardBack} alt="" />
+            </div>
+
+            <section className="cardGamesText">
+              <h3>CARD KEY</h3>
+              <ul className="cardKeyUl">  
+                <li>Player: Ace can be worth 11 or 1.</li>
+                <li>Dealer: First Ace counts as 11 unless it busts the hand. Subsequent Aces count as 1.</li>
+                <li>King, Queen, and Jack are worth 10 points each.</li>
+                <li>All other cards take on their numerical value.</li> 
+              </ul>
+            </section>
+
+
           </div>
 
-          <div className="cardGamesText">
-            <h3>CARD KEY</h3>
-            <ul>  
-              <li>Ace can be worth 11 or 1.</li>
-              <li>King, Queen, and Jack are worth 10 points each.</li>
-              <li>All other cards take on their numerical value.</li> 
-            </ul>
-          </div>
+          <ul className='buttonGuide'>
+            <li>
+              <button>HIT</button>
+              <p>Draw an additional card.</p>
+            </li>
+            <li>
+              <button>STAND</button>
+              <p>End your turn</p>
+            </li>
+          </ul>
+
         </div>
-        
 
       </div>
+
+
       <button onClick={closeHowToPlay}>OK</button>
     </div>
   );
